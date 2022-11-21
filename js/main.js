87e0d8a297af6265a8ec57d3ddbe39febc5821a0
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-21 17:03:58 +0100

    add css

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ createApp({
     data() {
         return {
             title: "Introduzione a Vue",
-            logo: "https://cdn.iconscout.com/icon/free/png-256/vue-282497.png"
+            logo: "https://cdn.iconscout.com/icon/free/png-256/vue-282497.png",
+            
         };
+    },
+    methods: {
     }
 }).mount('#app')
